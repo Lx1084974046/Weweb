dbd8ee5a41725c40da83de303d57ff6dd03df699
Lx1084974046 <1084974046@qq.com>
2022-02-09 20:08:19 +0800

    config

--- src/views/dashboard/modules/create/index.vue
+++ src/views/dashboard/modules/create/index.vue
@@ -1,50 +1,101 @@
 <template>
   <div class="module-create">
-    <div class="create-btn">
+    <div class="create-btn" @click="showModel">
       <plus-outlined />
       <span>新增社区模版</span>
     </div>
+    <a-card
+      hoverable
+      class="card-item"
+      @click="router.push({ name: 'config', query: { id: 456 } })"
+    >
+      <template #cover>
+        <i class="img-logo"></i>
+      </template>
+      <a-card-meta title="方案一">
+        <template #description>
+          <div class="desc-box">
+            <!-- <a-tag color="purple" class="tag-des">
+              <span>创建时间:</span>
+              {{ 123 }}
+            </a-tag> -->
+            <a-tag color="orange" class="tag-des">
+              <span>专题Code:</span>
+              {{ 'dasdasdasdas' }}
+            </a-tag>
+          </div>
+        </template>
+      </a-card-meta>
+    </a-card>
+    <a-modal
+      v-model:visible="modelVisible"
+      title="新增社区模版"
+      :confirm-loading="confirmLoading"
+      cancelText="取消"
+      okText="确定"
+      @ok="addOkChange"
+    >
+      <a-form ref="formRef" :model="modalFormState" :rules="rules">
+        <a-form-item ref="name" label="微社区名称" name="name">
+          <a-input
+            v-model:value="modalFormState.name"
+            placeholder="请选择产品名称"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </div>
 </template>
 
 <script>
-import { PlusOutlined} from '@ant-design/icons-vue';
+import { useRouter } from 'vue-router';
+import { PlusOutlined } from '@ant-design/icons-vue';
 import { reactive, toRefs, onMounted } from 'vue';
 export default {
   name: '',
-  components:{
+  components: {
     PlusOutlined
   },
   setup() {
-    const state = reactive({});
+    const router = useRouter();
+    const state = reactive({
+      modalFormState: {},
+      modelVisible: false
+    });
+    const rules = {
+      name: [
+        {
+          required: true,
+          message: '请输入微社区模版名称',
+          trigger: 'blur'
+        }
+      ]
+    };
+    const addOkChange = () => {};
+    const showModel = () => {
+      state.modelVisible = true;
+    };
     onMounted(() => {
       console.log('3.-组件挂载到页面之后执行-------onMounted');
     });
     return {
+      router,
+      rules,
+      showModel,
       ...toRefs(state)
     };
   }
 };
 </script>
-<style lang="less" scoped>
-.module-create{
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-content: flex-start;
-  .create-btn{
-    width: 300px;
-    height: 200px;
-    border: 1px dashed #ccc;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    span{
-      margin-left: 2px;
-    }
-  }
+<style lang="less">
+.ant-card-body {
+  padding: 5px;
 }
+.ant-card-meta-title {
+  text-align: left;
+  margin-bottom: 0;
+}
+</style>
+<style lang="less" scoped>
+@import './index.less';
 </style>
